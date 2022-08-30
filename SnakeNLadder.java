f9060166b4d6d6c7154aeaaa87aa152d9ccc5960
iamsaqlain_q <iamkhatik786@gmail.com>
2022-08-30 19:40:09 +0530

    Snake and Ladder

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -2,11 +2,16 @@ package com.bl.snakenladder;
 
 public class SnakeNLadder {
 	
-	
+	public static final int POSITION = 0;
 	
 	public static void main(String args[]) {
 		
 		System.out.println("Snake And Ladder Simulator");
 		
+		byte player = 1;
+
+		System.out.println("Game Starts...");
+		System.out.println("Number of Players " + player);
+		System.out.println("Position : On Square " + POSITION);
 	}
 }
